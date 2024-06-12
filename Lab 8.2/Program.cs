c1845dd746c21cf21ce0caa035a727d6a4824524
neomeomeo <haonguyentayson@gmail.com>
2024-06-12 12:51:14 +0700

    first commit

---
+++ Lab 8.2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+public class Deadlock
+{
+    static object lock1 = new object();
+    static object lock2 = new object();
+
+    static void Thread1FunctionLab()
+    {
+        if (Monitor.TryEnter(lock1, TimeSpan.FromMilliseconds(1000)))
+        {
+            try
+            {
+                Console.WriteLine("Thread 1 acquired lock 1");
+                Thread.Sleep(100);
+                Console.WriteLine("Thread 1 is waiting for lock2");
+
+                if (Monitor.TryEnter(lock2, TimeSpan.FromMilliseconds(1000)))
+                {
+                    try
+                    {
+                        Console.WriteLine("Thread 1 acquired lock 2");
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lock2);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Thread 1 failed to acquire lock 2");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(lock1);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Thread 1 failed to acquire lock 1");
+        }
+    }
+
+    static void Thread2FunctionLab()
+    {
+        if (Monitor.TryEnter(lock2, TimeSpan.FromMilliseconds(1000)))
+        {
+            try
+            {
+                Console.WriteLine("Thread 2 acquired lock 2");
+                Thread.Sleep(100);
+                Console.WriteLine("Thread 2 is waiting for lock1");
+
+                if (Monitor.TryEnter(lock1, TimeSpan.FromMilliseconds(1000)))
+                {
+                    try
+                    {
+                        Console.WriteLine("Thread 2 acquired lock 1");
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lock1);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Thread 2 failed to acquire lock 1");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(lock2);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Thread 2 failed to acquire lock 2");
+        }
+    }
+
+    public void bad2()
+    {
+        Thread thread1 = new Thread(Thread1FunctionLab);
+        Thread thread2 = new Thread(Thread2FunctionLab);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("end start");
+    }
+}
---
+++ Lab 8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static List<int> numbers = new List<int>();
+
+    static void Thread1Function()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            int randomNumber = new Random().Next(1, 11);
+            numbers.Add(randomNumber);
+            Console.WriteLine($"Thread 1: Generated random number {randomNumber}");
+            Thread.Sleep(2000); // sleep for 2 seconds
+        }
+    }
+
+    static void Thread2Function()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (numbers.Count > 0)
+            {
+                int number = numbers[0];
+                numbers.RemoveAt(0);
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine($"Thread 2: Square root of {number} is {squareRoot}");
+            }
+            Thread.Sleep(1000); // sleep for 1 second
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(Thread1Function);
+        Thread thread2 = new Thread(Thread2Function);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("All threads finished");
+    }
+}
